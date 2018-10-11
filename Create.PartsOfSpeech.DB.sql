a05c6aa9c1d80eb5d730b0fc8e4469c57e1b0870
Joy Harvel <geekgirl.joy@gmail.com>
2018-10-10 22:13:47 -0700

    Update Create.PartsOfSpeech.DB.sql

--- Create.PartsOfSpeech.DB.sql
+++ Create.PartsOfSpeech.DB.sql
@@ -1,6 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 --
 -- Database: `PartsOfSpeechTagger`
 --
@@ -9,14 +6,16 @@ USE `PartsOfSpeechTagger`;
 
 -- --------------------------------------------------------
 
+-- --------------------------------------------------------
+
 --
--- Table structure for table `Dictionary`
+-- Table structure for table `Tags`
 --
 
-CREATE TABLE `Dictionary` (
+CREATE TABLE `Tags` (
   `ID` int(11) NOT NULL,
-  `Word` varchar(100) NOT NULL,
-  `Tags` text NOT NULL
+  `Tag` varchar(8) NOT NULL,
+  `Definition` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -28,70 +27,65 @@ CREATE TABLE `Dictionary` (
 CREATE TABLE `Trigrams` (
   `ID` int(11) NOT NULL,
   `Count` int(11) NOT NULL,
-  `Word_A` varchar(200) NOT NULL,
-  `Word_B` varchar(200) NOT NULL,
-  `Word_C` varchar(200) NOT NULL,
-  `Tag_A` varchar(50) NOT NULL,
-  `Tag_B` varchar(50) NOT NULL,
-  `Tag_C` varchar(50) NOT NULL
+  `Word_A` int(11) NOT NULL,
+  `Word_B` int(11) NOT NULL,
+  `Word_C` int(11) NOT NULL,
+  `Tag_A` int(11) NOT NULL,
+  `Tag_B` int(11) NOT NULL,
+  `Tag_C` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Sources`
+-- Table structure for table `Words`
 --
 
-CREATE TABLE `Sources` (
+CREATE TABLE `Words` (
   `ID` int(11) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `Genre` varchar(255) NOT NULL,
-  `Keywords` text NOT NULL,
-  `Description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+  `Word` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Dictionary`
+-- Indexes for table `Tags`
 --
-ALTER TABLE `Dictionary`
+ALTER TABLE `Tags`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Word` (`Word`);
+  ADD UNIQUE KEY `Tag` (`Tag`);
 
 --
 -- Indexes for table `Trigrams`
 --
 ALTER TABLE `Trigrams`
   ADD PRIMARY KEY (`ID`);
-  
+
 --
--- Indexes for table `Sources`
+-- Indexes for table `Words`
 --
-ALTER TABLE `Sources`
+ALTER TABLE `Words`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Word` (`Name`);
-  
+  ADD UNIQUE KEY `Word` (`Word`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Dictionary`
+-- AUTO_INCREMENT for table `Tags`
 --
-ALTER TABLE `Dictionary`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Tags`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `Trigrams`
 --
 ALTER TABLE `Trigrams`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `Sources`
+-- AUTO_INCREMENT for table `Words`
 --
-ALTER TABLE `Sources`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Words`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
